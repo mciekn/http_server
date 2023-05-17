7513ad44b34c9bc21f6c98ef7c3c5777e0c7d435
Maciej Niszczota <macni.macni@gmail.com>
2023-05-17 23:21:23 +0200

    Implement basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream){
+    let mut data = [0 as u8; 50];
+    while match stream.read(&mut data){
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occured, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:2137").unwrap();
+    println!("Server listening on port 2137");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+    drop(listener);
 }
