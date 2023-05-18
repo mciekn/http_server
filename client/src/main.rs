8102f418009818d35aa2472ca9ee0a2bc6daf80b
Maciej Niszczota <macni.macni@gmail.com>
2023-05-18 20:33:38 +0200

    Add simple client, change repo structure

---
+++ client/src/main.rs
@@ -0,0 +1,37 @@
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
+fn main(){
+    match TcpStream::connect("127.0.0.1:2137"){
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port 2137");
+
+            let msg = b"Hello ";
+
+            stream.write(msg).unwrap();
+            println!("Sent hello, awaiting reply...");
+
+            let mut data = [0 as u8; 6];
+            match stream.read_exact(&mut data){
+                Ok(_) => {
+                    if &data == msg {
+                        println!("reply is ok");
+                    }
+                    else{
+                        let text = from_utf8(&data).unwrap();
+                        println!("Unexpected reply: {}", text);
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Terminated.")
+}
